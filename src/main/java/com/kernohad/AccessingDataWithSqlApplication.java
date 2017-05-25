0763881c2e6464a30ae69ee6fbe17fd5958dc4ee
Dylan Kernohan <kernohad@mail.gvsu.edu>
2017-05-25 16:42:05 -0400

    Basic create read update

---
+++ src/main/java/com/kernohad/AccessingDataWithSqlApplication.java
@@ -0,0 +1,18 @@
+package com.kernohad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * This project was made to explore accessing data using MySQL
+ * Week 2 at KL&A
+ * 5/24/2017
+ */
+@SpringBootApplication
+public class AccessingDataWithSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataWithSqlApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kernohad/UserRepository.java
@@ -0,0 +1,20 @@
+package com.kernohad;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import com.kernohad.User;
+import org.springframework.data.repository.query.Param;
+
+import java.util.ArrayList;
+
+/**
+ * This is the repository interface, this will be automatically
+ *  implemented by Spring in a bean with the same name with changing
+ *  case The bean name will be userRepository
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("select u from User u where u.name = :name and u.email = :email")
+    User findUser(@Param("name") String name, @Param("email") String email);
+
+
+}
